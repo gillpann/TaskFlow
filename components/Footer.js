cc57ffa2e2e078de82dacb569e0370fccd1ef14a
gillpann <cpundong@gmail.com>
2025-01-29 15:22:16 +0700

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,14 @@
 export default function Footer() {
+  const gotoMyPortfolio = () => {
+    window.open("https://ahmad-gilvan.vercel.app", "_blank");
+  };
   return (
     <footer className="bg-white border-t mt-auto py-6">
       <div className="container mx-auto px-4 text-center text-gray-600">
-        <p>
-          &copy; {new Date().getFullYear()} Created by Gilvan. All rights reserved.
-        </p>
+        <p>&copy; {new Date().getFullYear()}, TaskFlow.</p>
+            <span>Made with ❤️ by  
+                <a target="_blank" rel="noopener noreferrer" onClick={gotoMyPortfolio}> Ahmad Gilvan</a>
+            </span>
       </div>
     </footer>
   );
