f8f42dcda00b849bed1825a9afef0d62e8dfcb67
gillpann <cpundong@gmail.com>
2025-01-29 15:23:56 +0700

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
       <div className="container mx-auto px-4 text-center text-gray-600">
         <p>&copy; {new Date().getFullYear()}, TaskFlow.</p>
             <span>Made with ❤️ by  
-                <a target="_blank" rel="noopener noreferrer" onClick={gotoMyPortfolio}> Ahmad Gilvan</a>
+                <a target="_blank" rel="noopener noreferrer" onClick={gotoMyPortfolio}> Ahmad Gilvan.</a>
             </span>
       </div>
     </footer>
